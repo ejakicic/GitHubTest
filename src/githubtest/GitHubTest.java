1f56d5c56b680e33be1f72ae979793993aeb3cae
ejakicic <56897378+ejakicic@users.noreply.github.com>
2020-02-09 13:36:29 -0500

    First Commit

---
+++ src/githubtest/GitHubTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubtest;
+
+/**
+ *
+ * @author ejaki
+ */
+public class GitHubTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Jakicic();
+    }
+    
+    public static void Jakicic(){
+        System.out.println("Evan Jakicic");
+    }
+    
+}
